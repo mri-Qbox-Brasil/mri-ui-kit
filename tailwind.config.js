a369c880bdae704e7c71c35575664449454dde0c
Murai Dev <89431075+mur4i@users.noreply.github.com>
2026-05-07 15:37:46 -0300

    feat: font saira

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ export default {
                 sm: 'calc(var(--radius) - 4px)'
             },
             fontFamily: {
-                sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'sans-serif'],
+                sans: ['Saira', 'ui-sans-serif', 'system-ui', '-apple-system', 'sans-serif'],
             }
         }
     },
